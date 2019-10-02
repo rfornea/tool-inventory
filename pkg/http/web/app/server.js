5d2eaf0f2d2f066a4f1e5bc2fb8ed94e35f13a4e
Rebel Fornea <rebelfornea@gmail.com>
2019-10-01 22:40:48 -0400

    update server.js

--- pkg/http/web/app/server.js
+++ pkg/http/web/app/server.js
@@ -1,8 +1,10 @@
 // server.js
-const express = require('express');
+const express = require("express");
+let history = require("connect-history-api-fallback");
 const serveStatic = require("serve-static");
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+const path = require("path");
+let app = express();
+app.use(history());
+app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 80;
 app.listen(port);
